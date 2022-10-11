7160d8715cc9d622781e799668f69b3bbac294a4
Samuel Veloso Marçal <svm@lionx.com.br>
2022-10-11 12:08:26 -0300

    feat: added_email_validated_false_field

--- tests/src/domain/test_model.py
+++ tests/src/domain/test_model.py
@@ -4,4 +4,5 @@ from tests.src.services.stubs import stub_user_model
 
 def test_when_init_model_then_return_expected_values():
     assert stub_user_model.is_active_user is False
+    assert stub_user_model.email_validated is False
     assert stub_user_model.must_to_first_login is True
